286b184377ef5d13e43f1cc9f295df0975aeb5d8
AkshaySharma008 <akshaysharma30121999@gmail.com>
2020-11-28 18:20:28 +0530

    basic application completed

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const { Socket } = require('socket.io');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app)
-const io = require("socket.io")(http)
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = 3000;
 
-const PORT = 3000;
+app.use("/", express.static(__dirname + "/public"));
 
-app.use("/" , express.static(__dirname + "/public"))
+const onConnection = socket => {
+  socket.on("drawing", data => socket.broadcast.emit("drawing", data));
+};
 
-const onConnection  = socket => {
-    socket.on("drawing" , data => socket.broadcast.emit("drawing" , data))
-}
-io.on("connection" , onConnection)
+io.on("connection", onConnection);
 
-http.listen(PORT , () => {
-    console.log("Server Started at Port" + PORT + ".")
-})
+http.listen(port, () => {
+  console.log("Server started in port " + port + ".");
+});
--- public/script.js
+++ public/script.js
@@ -1,85 +1,112 @@
-//initialized socket and get context of canvas
+// Initialise Socket & Get Context of Canvas.
 var socket = io();
-var canvas = document.querySelector(".whiteboard")
-var context = canvas.getContext("2d")
+var canvas = document.querySelector(".whiteboard");
+var context = canvas.getContext("2d");
 
-//configuration
-var isDrawing = false;
+// Configuration.
+var drawing = false;
 var current = {
-    color:"black"
-}
+  color: "black"
+};
 
+function throttle(callback, delay) {
+  var previousCall = new Date().getTime();
+  return function () {
+    var time = new Date().getTime();
 
-//remove extra data
-function trottle(callback , delay){
-    var previousCall = new Date().getTime();
-    return function(){
-        var time = new Date().getTime();
-        if(time - previousCall >= delay){
-            previousCall = time;
-            callback.apply(null ,arguments);
-        }
+    if (time - previousCall >= delay) {
+      previousCall = time;
+      callback.apply(null, arguments);
     }
-
+  };
 }
 
-function drawLine(x0,y0,x1,y1 , color , emit){
-    context.beginPath();
-    context.moveTo(x0,y0);
-    context.lineTo(x1,y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2
-    context.stroke();
-    context.closePath();
+function drawLine(x0, y0, x1, y1, color, emit) {
+  context.beginPath();
+  context.moveTo(x0, y0);
+  context.lineTo(x1, y1);
+  context.strokeStyle = color;
+  context.lineWidth = 2;
+  context.stroke();
+  context.closePath();
 
-    if(!emit)return;
+  if (!emit) {
+    return;
+  }
 
-    var w = canvas.width;
-    var h = canvas.height;
+  var w = canvas.width;
+  var h = canvas.height;
 
-    socket.emit("drawing" , {
-        x0 : x0/w,
-        y0 : y0/h,
-        x1 : x1/w,
-        y1 : y1/h,
-        color
-    })
+  socket.emit("drawing", {
+    x0: x0 / w,
+    y0: y0 / h,
+    x1: x1 / w,
+    y1: y1 / h,
+    color
+  });
 }
 
-
-//handlers --mouse/touch events (up/out/down/move) (start/ end or cancel /move)
-
-function onMouseDown(e){
-    isDrawing = true;
-    current.x = e.clientX || e.touches[0].clientX;
-    current.y = e.clientY || e.touches[0].clientY;
+function onMouseDown(e) {
+  drawing = true;
+  current.x = e.clientX || e.touches[0].clientX;
+  current.y = e.clientY || e.touches[0].clientY;
 }
 
-function onMouseUp(e){
-    if(!isDrawing) return;
-    isDrawing = false;
-    drawLine(current.x ,
-        current.y ,
-        e.clientX || e.touches[0].clientX , 
-        e.clientY || e.touches[0].clientY , 
-        current.color , 
-        true
-         );
-
+function onMouseUp(e) {
+  if (!drawing) {
+    return;
+  }
+  drawing = false;
+  drawLine(
+    current.x,
+    current.y,
+    e.clientX || e.touches[0].clientX,
+    e.clientY || e.touches[0].clientY,
+    current.color,
+    true
+  );
 }
 
-function onMouseMove(e){
-    if(!isDrawing) return;
-    drawLine(current.x ,
-         current.y ,
-         e.clientX || e.touches[0].clientX , 
-         e.clientY || e.touches[0].clientY , 
-         current.color , 
-         true
-          );
-
-    current.x = e.clientX || e.touches[0].clientX;
-    current.y = e.clientY || e.touches[0].clientY;
-    
+function onMouseMove(e) {
+  if (!drawing) {
+    return;
+  }
+  drawLine(
+    current.x,
+    current.y,
+    e.clientX || e.touches[0].clientX,
+    e.clientY || e.touches[0].clientY,
+    current.color,
+    true
+  );
+  current.x = e.clientX || e.touches[0].clientX;
+  current.y = e.clientY || e.touches[0].clientY;
 }
 
+// Desktop Events
+canvas.addEventListener("mousedown", onMouseDown, false);
+canvas.addEventListener("mouseup", onMouseUp, false);
+canvas.addEventListener("mouseout", onMouseUp, false);
+canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
+
+// Mobile Events
+canvas.addEventListener("touchstart", onMouseDown, false);
+canvas.addEventListener("touchend", onMouseUp, false);
+canvas.addEventListener("touchcancel", onMouseUp, false);
+canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
+
+function onResize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", onResize, false);
+onResize();
+
+function onDrawingEvent(data) {
+  var w = canvas.width;
+  var h = canvas.height;
+  drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+}
+
+socket.on("drawing", onDrawingEvent);
